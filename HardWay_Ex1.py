f53d6a8b6dfde769511d6b6be912fee8956daa74
edelac13 <edelac13@ford.com>
2019-08-20 10:51:57 -0500

    Learning Python the Hard Way. Exercise 1 y 2.

---
+++ HardWay_Ex1.py
@@ -0,0 +1,11 @@
+# This Exercise was intended to learn how to print in shell, to be carefully about open an close of strings ("" or '')
+# Also introduces the use of the hash character (#) as a way to comment text.
+
+# print ("Hello World!")
+# print ("Hello Again")
+# print ("I like typing this.")
+# print ("This is fun.")
+# print ('Yay! Printing.')
+# print ("I'd much rather you 'not'.")
+# print ('I "said" do not touch this.')
+print ("with this, I'm finally closing this exercise")
