3d855a1c07a86a8f5e0892f2c5bcef97a6322f4d
edelac13 <edelac13@ford.com>
2019-08-21 15:33:25 -0500

    Learning Python the Hard Way. Exercise 3 y 4.

---
+++ HardWay_Ex3.py
@@ -0,0 +1,24 @@
+print ("I will now count my chickens:")
+
+print ("Hens", 25 + 30 / 6)  # 30?
+print ("Roosters", 100 - 25 * 3 % 4) # 97?
+
+print ("Now I will count the eggs:")
+
+print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) # 1+ 0 -0.25 + 6 = 6.75
+
+print("Is it true that 3 + 2 < 5 -7?")
+
+# Aqui aprendemos que tiene precedencia las sumas aritmeticas que las comparaciones.(3+2)<(5-7)
+print (3 + 2 < 5 -7) # False!!!
+
+print ("What is 3 + 2?", 3 + 2)
+print ("What is 5 -7?", 5 - 7)
+
+print("Oh, that's why it's false.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2) # TRUE
+print("Is it greater or equal?", 5 >= -2) # TRUE
+print("Is it less or equal", 5 <= -2) # False
