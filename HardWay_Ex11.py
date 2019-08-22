d75b0a372e8982f7b9f1215e9df2405bf839511d
edelac13 <edelac13@ford.com>
2019-08-22 15:10:11 -0500

    Learn Python the Hard Way. Exercise 11 y 12

---
+++ HardWay_Ex11.py
@@ -0,0 +1,12 @@
+print("")
+
+print("How old are you?", end =' ') #end = ' ' permite no terminar en otra fila, mantenerse en la linea y agrega un espacio antes de escribir
+age = input()
+print("How tall are you?", end =' ')
+height = input()
+print("How much do you weight?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+print("")
